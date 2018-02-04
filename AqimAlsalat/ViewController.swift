84e476b528d8c5a6e5cf475ddae8de0d48450040
Dalal Mansour <dalalmansour@Dalals-MacBook-Pro.local>
2018-02-04 09:32:25 +0300

    lbl

--- AqimAlsalat/ViewController.swift
+++ AqimAlsalat/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+class ViewController: UIViewController ,CLLocationManagerDelegate{
+   let locationManager = CLLocationManager()
+    
+    
+    
+    @IBOutlet var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
-
+    
+    //implemintation
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first
+        {
+            var userLocation = location.coordinate
+            print(userLocation)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if( status == CLAuthorizationStatus.denied){
+            showLocationDisable()
+        }
+    }
+   /* func showLocationDisable(){
+        let alertA = UIAlertController(title: "your GPS is turned off", message: "we need your location", preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        alertA.addAction(cancel)
+        let openA = UIAlertAction(title: "oopen", style: .default) {(action) in
+            if let url = URL(string: UIApplicationOpenSettingsURLString){
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        alertA.addAction(openA)
+        self.present(alertA, animated: true, completion: nil)
+    }*/
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
