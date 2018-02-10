0f6648fb36c2662d69d379aeeaf126640cb39d18
Dalal Mansour <dalalmansour@dalals-macbook-pro.local>
2018-02-10 12:08:12 +0300

    add

--- AqimAlsalat/ViewController.swift
+++ AqimAlsalat/ViewController.swift
@@ -1,66 +1,137 @@
+//  locationManager.requestAlwaysAuthorization()
 //
 //  ViewController.swift
-//  AqimAlsalat
+//  First
 //
-//  Created by ساره عبدالله on ١٣ جما١، ١٤٣٩ هـ.
-//  Copyright © ١٤٣٩ هـ ساره عبدالله. All rights reserved.
+//  Created by Dalal Mansour on 2/9/18.
+//  Copyright © 2018 Dalal Mansour. All rights reserved.
 //
 
 import UIKit
-import CoreLocation
-class ViewController: UIViewController ,CLLocationManagerDelegate{
-   let locationManager = CLLocationManager()
+import GoogleMaps
+
+class ViewController: UIViewController,CLLocationManagerDelegate {
     
+    //constants and variables
+    lazy var mapView = GMSMapView()
+    var locationManager = CLLocationManager()
+    var camera = GMSCameraPosition.camera(withLatitude: 24.725239, longitude: 46.637492, zoom: 13.0)
+    var  userLocation = CLLocation()
     
+    //markers locations
+    var userLocation1 =  CLLocation(latitude: 24.723561, longitude: 46.622433)
+    var userLocation2 =  CLLocation(latitude: 24.713739, longitude: 46.613850)
+    var userLocation3 =  CLLocation(latitude: 24.715295, longitude: 46.620974)
     
-    @IBOutlet var lbl: UILabel!
+    //distance
+    var distanceInMeters, distanceInMeters1, distanceInMeters2 :Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        locationManager.requestAlwaysAuthorization()
         
-        if CLLocationManager.locationServicesEnabled(){
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            locationManager.startUpdatingLocation()
-        }
-    }
-    // sarah 
-  //dalal
-    //implemintation
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first
+        //user location
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        
+        //create priority queue
+        
+        
+        var nodes = PriorityQueue<Mosque>(sort: { $0.priority < $1.priority })
+        
+        // create distance and nodes
+        
+        //distance
+        let distanceInMeters = userLocation.distance(from: userLocation1)
+        let distanceInMeters2 = userLocation.distance(from: userLocation2)
+        let distanceInMeters3 = userLocation.distance(from: userLocation3)
+        
+        
+        
+        
+        //nodes
+        let m =  Mosque(priority: distanceInMeters, name: "a",location:(userLocation2), status:"green")
+        let  m1 = Mosque(priority: distanceInMeters2, name: "b",location:(userLocation3), status:"orange")
+        let m2 = Mosque(priority: distanceInMeters3, name: "c",location:(userLocation1), status:"green")
+        
+        
+        print("11111a:\(distanceInMeters)")
+        print("22222b:\(distanceInMeters2)")
+        print("333333c:\(distanceInMeters3)")
+        
+        //peek Node
+        var peekNode = Mosque(priority: 1, name: "dalal", location: (userLocation1), status:"grey")
+        
+        if(m.status != "red")
         {
-            var userLocation = location.coordinate
-            print(userLocation)
+            nodes.enqueue(element: m)
+            nodes.enqueue(element: m1)
+            nodes.enqueue(element: m2)
+            
+            peekNode = nodes.peek()!
+            print(" * Node(priority: \(peekNode.priority))"+" * Node(names: \(peekNode.location.altitude))"+" * Node(names: \(peekNode.name))")
+            
         }
+        else{
+            print("reeed")
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if( status == CLAuthorizationStatus.denied){
-            //showLocationDisable()
-        }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        userLocation = locations.last!
+        _ = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        
+        camera = GMSCameraPosition.camera(withLatitude: userLocation.coordinate.latitude,
+                                          longitude: userLocation.coordinate.longitude, zoom: 13.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 24.723561, longitude: 46.622433)
+        marker.title = "mosque1"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+        
+        let markerSquirt = GMSMarker()
+        markerSquirt.position = CLLocationCoordinate2D(latitude: 24.713739, longitude: 46.613850)
+        markerSquirt.title = "mosque2"
+        markerSquirt.snippet = "Squirtle lives here"
+        markerSquirt.map = mapView
+        
+        let markerSquirt2 = GMSMarker()
+        markerSquirt2.position = CLLocationCoordinate2D(latitude: 24.715295, longitude: 46.620974)
+        markerSquirt2.title = "mosque3"
+        markerSquirt2.snippet = "Squirtle lives here"
+        markerSquirt2.map = mapView
+        
+        
+        
+        
+        locationManager.stopUpdatingLocation()
     }
-   /* func showLocationDisable(){
-        let alertA = UIAlertController(title: "your GPS is turned off", message: "we need your location", preferredStyle: .alert)
-        
-        let cancel = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
-        
-        alertA.addAction(cancel)
-        let openA = UIAlertAction(title: "oopen", style: .default) {(action) in
-            if let url = URL(string: UIApplicationOpenSettingsURLString){
-                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-            }
-        }
-        alertA.addAction(openA)
-        self.present(alertA, animated: true, completion: nil)
-    }*/
+    
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AqimAlsalat' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AqimAlsalat
+     pod 'GoogleMaps'
+   pod 'GooglePlaces'
+
+  target 'AqimAlsalatTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'AqimAlsalatUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
